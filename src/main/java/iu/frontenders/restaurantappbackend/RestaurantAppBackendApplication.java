bc9294990fe48962058a119fac39619bd976db7f
almax07082005 <almaxim07082005@gmail.com>
2024-06-23 15:07:13 +0300

    init commit

---
+++ src/main/java/iu/frontenders/restaurantappbackend/RestaurantAppBackendApplication.java
@@ -0,0 +1,13 @@
+package iu.frontenders.restaurantappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantAppBackendApplication.class, args);
+	}
+
+}
